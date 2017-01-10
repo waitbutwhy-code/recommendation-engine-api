83127227ac6ea7f0fc41b0ba6e8494555cb4cc11
= <=>
2017-01-10 18:41:45 +0000

    completed initial rater.add function

--- api/rater.js
+++ api/rater.js
@@ -7,15 +7,39 @@ class Rater {
         this.db = new Bourne(`./db-${type}.json`);
     }
 
+/*
+Add function to add a user
+1. check if user exists (find user in db)
+2. if user exist , do not do anything
+3. if user does not exist , add user
+4. then calculate similar users and suggestions
+*/
     add(user){
-        console.log(`Add user of type ${this.type}`);
+        return new Promise((resolve, reject) => {
+            this.db.find({user}, (err, res) => {
+                if (err) {
+                    reject('Error finding user in db: ',err);
+                }
+                if (res.length > 0) {
+                    resolve('User already exists');
+                }
 
+                this.db.insert({user}, (err, res) => {
+                    if (err) {
+                        reject('Error inserting user in db: ',err);
+                    }
+
+                    // Update similar users here 
+                    // Update suggestions here
+                    
+                    resolve('User added');
+                })
+            })
+        })
     }
 }
 
-/*
-Add function to add a user
-*/
+
 
 module.exports = {
     Rater,
